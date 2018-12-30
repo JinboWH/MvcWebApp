6c32e886fc1cdca887cb4f100a79cd8f13e09d69
金波 吴hjjinboadfdf <jbwu.wu@hotmail.com>
2018-12-30 11:41:14 +0800

    mvc

---
+++ MvcWebApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace MvcWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //CreateWebHostBuilder(args).Build().Run();
+            IWebHostBuilder webHostBuider = CreateWebHostBuilder(args);
+            //webHostBuider.Build().Run();
+            IWebHost webHost = webHostBuider.Build();
+            webHost.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
